4b14dcf0a09aab8885e22a72b12648f1ec445293
Derrick Ellerbie <derrick@Derricks-MacBook-Pro.local>
2008-11-01 21:31:54 -0400

    Functional test for updating password

--- test/functional/account_controller_test.rb
+++ test/functional/account_controller_test.rb
@@ -1,8 +1,27 @@
 require 'test_helper'
 
 class AccountControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :users
+  
+  def setup
+    @quentin = users(:quentin)
   end
+  
+  def test_should_change_password_and_redirect_to_edit_page
+    login_as :quentin
+    post :update_password, {:user => {:old_password => 'monkey', :password => '111111', :password_confirmation => '111111'}}
+    assert_not_nil assigns(:user)
+    assert_response :redirect
+    assert_equal "Password successfully updated.", flash[:notice]
+  end
+  
+  def test_should_not_change_password
+    login_as :quentin
+    post :update_password, {:user => {:old_password => 'abc123', :password => '111111', :password_confirmation => '111111'}}
+    assert_not_nil assigns(:user)
+    assert_response :success
+    assert_template 'edit'
+    assert_nil flash[:notice]
+  end
+  
 end
