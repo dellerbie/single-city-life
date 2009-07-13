dfb239243720dae08d2bea30567e6efa28fd75cb
Derrick Ellerbie <slowblues@gmail.com>
2009-07-12 20:56:11 -0400

    Birthdate filtering added

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -30,7 +30,13 @@ class IndexController < ApplicationController
   def filter
     query = User.query
     query.and.gender_in(params[:gender]) if params[:gender]
-    query.and.default_photo_id_is_not_null if params[:has_pic] == 'y'
+    query.and.default_photo_id_is_not_null if params[:has_pic] == 'y' 
+
+    age_ranges = get_age_ranges(params[:age_begin], params[:age_end])
+    unless age_ranges.empty?
+      query.and.birthdate_lte(age_ranges[:age_begin]).birthdate_gte(age_ranges[:age_end])
+    end
+    
     query.join(:profile)
     query.profile.and.best_feature_in(params[:best_feature]) if params[:best_feature]
     query.profile.and.body_type_in(params[:body_type]) if params[:body_type]
@@ -51,5 +57,18 @@ class IndexController < ApplicationController
       end
     end
     json
+  end
+  
+  def get_age_ranges(age_begin, age_end)
+    ranges = {}
+    if age_begin && age_end 
+      age_begin = age_begin.to_i
+      age_end = age_end.to_i
+      if((age_begin < age_end) && (age_begin >= 18))
+        ranges[:age_begin] = Date.today.years_ago(age_begin).beginning_of_year
+        ranges[:age_end] = Date.today.years_ago(age_end).beginning_of_year
+      end
+    end
+    ranges
   end
 end
